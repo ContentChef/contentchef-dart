ae5e0d473a53fd2e647f29df115fe7a2342b6719
Nicola Bellotti <n.bellotti90@gmail.com>
2020-04-24 09:39:01 +0200

    add client tests

---
+++ test/contentchef_test.dart
@@ -0,0 +1,32 @@
+import 'package:test/test.dart';
+import 'package:contentchef_dart/src/channels.dart';
+import 'package:contentchef_dart/contentchef_dart.dart';
+
+void main() {
+
+  group('ContentChef Client tests', () {
+    final validConfiguration = Configuration(spaceId: 'test-spaceId');
+    final validTargetDateResolver = TargetDateResolver(targetDateSource: null);
+    test('Initializing throw exception if a null configuration is given', () {
+      expect(() => ContentChef(configuration: null), throwsException);
+    });
+
+    test('Initialize succesfully without a targetDateResolver', () {
+      expect(ContentChef(configuration: validConfiguration), const TypeMatcher<ContentChef>());
+    });
+
+    test('Initialize succesfully with a targetDateResolver', () {
+      expect(ContentChef(configuration: validConfiguration, targetDateResolver: validTargetDateResolver), const TypeMatcher<ContentChef>());
+    });
+
+    test('return the configured onlineChannel', () {
+      final contentChef = ContentChef(configuration: validConfiguration, targetDateResolver: validTargetDateResolver);
+      expect(contentChef.getOnlineChannel(apiKey: 'test-apyKey', publishingChannel: 'test-channel'), const TypeMatcher<OnlineChannel>());
+    });
+
+    test('return the configured previewChannel', () {
+      final contentChef = ContentChef(configuration: validConfiguration, targetDateResolver: validTargetDateResolver);
+      expect(contentChef.getPreviewChannel(apiKey: 'test-apyKey', publishingChannel: 'test-channel', status: PublishingStatus.stage), const TypeMatcher<PreviewChannel>());
+    });
+  });
+}
