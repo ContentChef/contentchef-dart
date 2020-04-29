84b342c28688670c9b34168ef82b327764ea8657
Nicola Bellotti <n.bellotti90@gmail.com>
2020-04-29 13:15:37 +0200

    add a better http error handling

--- lib/src/request_executor.dart
+++ lib/src/request_executor.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:contentchef_dart/src/configuration.dart';
+import 'package:contentchef_dart/src/errors.dart';
 import 'package:contentchef_dart/src/requests_filters.dart';
 import 'package:contentchef_dart/src/responses.dart';
 import 'package:meta/meta.dart';
 
 /// ContentChef RequestExecutor class (internal use only)
+///
 class RequestExecutor {
 
   /// Resolve all api requests
@@ -17,14 +19,18 @@ class RequestExecutor {
   /// - filters: a map of filters used as queryParameters in the api request
   /// - Returns:
   ///   Success => returns the api response body as Json object if the response statusCode is >= 200 or < 299.
-  ///   Fail =>    returns an exception if the response statusCode is < 200 or > 299;
+  ///   Fail =>    returns a BadRequestException if the response statusCode is equal to 400;
+  ///              returns a ForbiddenException if the response statusCode is equal to 403;
+  ///              returns a NotFoundException if the response statusCode is equal to 404;
+  ///              returns a GenericErrorException otherwise
+  ///
   Future<Map<String, dynamic>> _executeRequest({
     @required String path,
     @required String apiKey,
     @required Configuration config,
     @required Map<String, String> filters,
   }) async {
-    var uri = Uri.https(config.host, path, filters);
+    var uri = Uri.https(config.host, '/dev$path', filters);
     var response = (await config.httpClient.get(uri, headers: {
       'X-SPACE-D-API-Key': apiKey,
     }).timeout(Duration(milliseconds: config.timeout)));
@@ -33,8 +39,21 @@ class RequestExecutor {
     if (response.statusCode >= 200 && response.statusCode <= 299) {
       return jsonDecode(response.body);
     }
-    print(response);
-    throw Exception('request failed with status code: ${response.statusCode}');
+
+    if (response.statusCode == 400) {
+      throw BadRequestException(
+          code: response.statusCode, error: jsonDecode(response.body));
+    }
+
+    if (response.statusCode == 403) {
+      throw ForbiddenException(code: response.statusCode);
+    }
+
+    if (response.statusCode == 404) {
+      throw NotFoundException(code: response.statusCode);
+    }
+
+    throw GenericErrorException(code: response.statusCode);
   }
 
   /// Resolve the getContent api request
@@ -46,7 +65,10 @@ class RequestExecutor {
   /// - filters: a map of filters used as queryParameters in the api request
   /// - Returns:
   ///     Success => returns an instance of ContentResponse<T> with payload(T) attribute of the defined type.
-  ///     Fail =>    returns an exception if the response statusCode is < 200 or > 299 or the mapping to ContentResponse<T> failed;
+  ///     Fail =>    returns a BadRequestException if the response statusCode is equal to 400;
+  ///                returns a ForbiddenException if the response statusCode is equal to 403;
+  ///                returns a NotFoundException if the response statusCode is equal to 404;
+  ///                returns a GenericErrorException otherwise
   Future<ContentResponse<T>> executeGetContentRequest<T>({
     @required String path,
     @required String apiKey,
@@ -70,7 +92,10 @@ class RequestExecutor {
   /// - filters: a map of filters used as queryParameters in the api request
   /// - Returns:
   ///     Success => returns an instance of PaginatedResponse<T> with items(ContentResponse<T>) attribute of the defined type.
-  ///     Fail =>    returns an exception if the response statusCode is < 200 or > 299 or the mapping to PaginatedResponse<T> failed;
+  ///     Fail =>    returns a BadRequestException if the response statusCode is equal to 400;
+  ///                returns a ForbiddenException if the response statusCode is equal to 403;
+  ///                returns a NotFoundException if the response statusCode is equal to 404;
+  ///                returns a GenericErrorException otherwise
   Future<PaginatedResponse<T>> executeSearchContentsRequest<T>({
     @required String path,
     @required String apiKey,
